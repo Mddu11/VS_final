d56fac1464a7c94664bfcd7f2430941c81ae556f
Mddu11 <vodilov_vano@mail.ru>
2026-06-18 02:48:10 +0300

    Delete app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routers import auth, teams, questions, games
-
-app = FastAPI(title="Quiz Service API")
-
-app.include_router(auth.router)
-app.include_router(teams.router)
-app.include_router(questions.router)
-app.include_router(games.router)
